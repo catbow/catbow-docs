f30232d5e28842a183946459a0daa884d893a6b4
최슬기 <choisk09@gmail.com>
2022-11-23 15:29:47 +0900

    wip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'catbow', // Usually your GitHub org/user name.
-  projectName: 'catbow.github.io', // Usually your repo name.
+  projectName: 'catbow-docs', // Usually your repo name.
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
